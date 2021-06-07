31c2663681d0b0668312835f3caf1118949ccae3
jessicashelley33 <85132275+jessicashelley33@users.noreply.github.com>
2021-06-07 12:53:34 -0400

    JS Checkup Assignment

---
+++ jsCheckup.js
@@ -0,0 +1,19 @@
+/*Assignment: JS Checkup */
+
+// Variables Assignment
+
+let num = 45;
+let firstName = "Jessica";
+let person = true;
+
+console.log(num);
+console.log(firstName);
+console.log(person);
+
+// Function Assignment
+
+function myFirstFunction () {
+    console.log("Hello, World!");
+    }
+
+    myFirstFunction();
